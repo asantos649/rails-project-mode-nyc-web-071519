563807763b7c9a3ea7b23c240e894ab33349324e
github username <github email address>
2019-08-15 17:26:40 -0400

    added login/sign up pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+    helper_method :user
+
+    def login
+        render "/login.html.erb"
+    end
+
+    def verification
+        # byebug
+        user = User.find_by(email: params[:email])
+        if user
+            session[:user] = user.id
+            redirect_to user_path(user)
+        end
+    end
+
+    def user
+        session[:user] ||= nil
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,22 +4,26 @@ class UsersController < ApplicationController
     end
 
     def new
-
+        @user = User.new
     end
 
     def create
-
+        @user = User.create(params.require(:user).permit(:name, :email))
+        redirect_to user_path(@user)
     end
 
     def edit
-
+        @user = User.find(params[:id])
     end
 
     def update
-
+        @user = User.find(params[:id])
+        @user.update(params.require(:user).permit(:name, :email))
+        redirect_to user_path(@user)
     end
 
     def destroy
-
+        User.find(params[:id]).destroy
+        # redirect to login page
     end
 end
